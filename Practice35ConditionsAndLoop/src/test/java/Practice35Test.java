f1cf2d0914a40da442880942d5f19bfa9e28c2aa
Gordei <gordeyvasilev@gmail.com>
2023-07-30 23:35:10 +1000

    Next lessons

---
+++ Practice35ConditionsAndLoop/src/test/java/Practice35Test.java
@@ -0,0 +1,71 @@
+import com.gordeyvasilev.Practice35;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+class Practice35Test {
+    @Test
+    @DisplayName("Should be processed \"-1\"")
+        // Redirect System.out to capture the output
+    void negativeNumber() {
+        String string = "-1";
+        InputStream stringStream = new ByteArrayInputStream(string.getBytes());
+
+        System.setIn(stringStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        // Call the main method
+        Practice35.main(new String[]{});
+
+        // Get the printed output
+        String printedOutput = outputStream.toString().trim();
+
+        // Assert that the output is "Hello world!"
+        Assertions.assertEquals("Seriously? What's so negative?", printedOutput);
+    }
+
+
+    @Test
+    @DisplayName("Should print 'Looks like nobody's here'")
+    void empty() {
+        String string = "0";
+        InputStream stringStream = new ByteArrayInputStream(string.getBytes());
+
+        System.setIn(stringStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        Practice35.main(new String[]{});
+
+        String printedOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals("Ooh... Looks like nobody's here...", printedOutput);
+    }
+
+
+    @Test
+    @DisplayName("Should print a greeting")
+    void greeting() {
+        String string = "2 Smith John";
+        InputStream stringStream = new ByteArrayInputStream(string.getBytes());
+
+        System.setIn(stringStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        Practice35.main(new String[]{});
+
+        String printedOutput = outputStream.toString().trim();
+
+        Assertions.assertEquals("Dear, Smith\nDear, John", printedOutput);
+    }
+}
