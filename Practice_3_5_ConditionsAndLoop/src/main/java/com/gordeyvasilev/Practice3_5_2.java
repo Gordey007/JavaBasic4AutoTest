53606c1555f6cd34e1fbdba5af30615b329ea5c5
Gordei <gordeyvasilev@gmail.com>
2023-08-31 23:57:26 +1000

    fix: ""

--- Practice_3_5_ConditionsAndLoop/src/main/java/com/gordeyvasilev/Practice3_5_2.java
+++ Practice_3_5_ConditionsAndLoop/src/main/java/com/gordeyvasilev/Practice3_5_2.java
@@ -8,15 +8,15 @@ import java.util.List;
  * Write a simple program that:
  * <ul>
  *   <li>Requests the number of strangers to meet, and their names separated by spaces.</li>
- *   <li>Outputs “Hello, (stranger's name)” for each stranger, line by line.</li>
+ *   <li>Outputs "Hello, (stranger's name)" for each stranger, line by line.</li>
  * </ul>
  * It is guaranteed that the number of strangers entered is an integer.
  * <br>
  * <br>
  * Special cases:
  * <ul>
- *   <li>If the number of strangers is zero, the program should output: “Oh... Looks like nobody's here...”.</li>
- *   <li>If the number of strangers is negative, the program should output: “Seriously? What's so negative?”.</li>
+ *   <li>If the number of strangers is zero, the program should output: "Oh... Looks like nobody's here...".</li>
+ *   <li>If the number of strangers is negative, the program should output: "Seriously? What's so negative?".</li>
  * </ul>
  *
  * @Task <a href="https://stepik.org/lesson/731261/step/2?unit=732798">#3.5</a>
