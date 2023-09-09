b6b0ed17e0e644197b0e6b24c372946a71e1f8dd
Gordei <gordeyvasilev@gmail.com>
2023-09-09 23:53:59 +1000

    chore: test CI

--- Theory_6_2_StaticAndFinal/src/main/java/com/gordeyvasilev/theory62staticandfinal/Demo2.java
+++ Theory_6_2_StaticAndFinal/src/main/java/com/gordeyvasilev/theory62staticandfinal/Demo2.java
@@ -4,20 +4,20 @@ import static com.gordeyvasilev.theory62staticandfinal.Car.numOfCars2_1;
 
 public class Demo2 {
     public static void main(String[] arg) {
-        Car сar1 = new Car();
-        Car сar2 = new Car();
-        Car сar3 = new Car();
+//        Car сar1 = new Car();
+//        Car сar2 = new Car();
+//        Car сar3 = new Car();
 
         /**
          * This happened because the numOfCars field is an instance field. Each object has its own copy of this
          * fields with an initial value of 0, which takes the value 1 during object creation. In this example it was
          * it would be more correct to make the numOfCars field static. Then it would be common to all objects.
          * */
-        System.out.println(сar2.getNumOfCars()); // 1
+//        System.out.println(сar2.getNumOfCars()); // 1
 
-        System.out.println(сar3.getNumOfCars2()); // 3
+//        System.out.println(сar3.getNumOfCars2()); // 3
 
-        System.out.println(numOfCars2_1);
-        System.out.println(Car.numOfCars2_2);
+//        System.out.println(numOfCars2_1);
+//        System.out.println(Car.numOfCars2_2);
     }
 }
